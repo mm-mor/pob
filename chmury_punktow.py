91622a20919eaf0e9e04051318d7d2d93b8e2eb3
mm_mor <mm_mor@onet.pl>
2023-03-13 19:48:00 +0100

    chmura punktow

--- chmury_punktow.py
+++ chmury_punktow.py
@@ -2,9 +2,22 @@ from scipy.stats import norm
 from csv import writer
 
 
-def generate_points(num_points: int = 2000):
-    distribution_x = norm(loc=0, scale=20)
-    distribution_y = norm(loc=0, scale=200)
+def generate_points_z(num_points: int = 2000):
+    distribution_x = norm(loc=0, scale=0.05)
+    distribution_y = norm(loc=0, scale=20)
+    distribution_z = norm(loc=0.2, scale=200)
+
+    x = distribution_x.rvs(size=num_points)
+    y = distribution_y.rvs(size=num_points)
+    z = distribution_z.rvs(size=num_points)
+
+    points = zip(x, y, z)
+    return points
+
+
+def generate_points_x(num_points: int = 2000):
+    distribution_x = norm(loc=0, scale=200)
+    distribution_y = norm(loc=0, scale=20)
     distribution_z = norm(loc=0.2, scale=0.05)
 
     x = distribution_x.rvs(size=num_points)
@@ -16,9 +29,13 @@ def generate_points(num_points: int = 2000):
 
 
 if __name__ == '__main__':
-    cloud_points = generate_points(2000)
+    cloud_points_x = generate_points_x(2000)
+    cloud_points_z = generate_points_z(2000)
     with open('LidarData.xyz', 'w', encoding='utf-8', newline='\n') as csvfile:
         csvwriter = writer(csvfile)
 
-        for p in cloud_points:
+        for p in cloud_points_x:
+            csvwriter.writerow(p)
+
+        for p in cloud_points_z:
             csvwriter.writerow(p)
